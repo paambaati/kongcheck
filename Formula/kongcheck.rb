91d27d78f1a3c16d901f7f3fc6d0d3f58f861d2e
Ganesh GP Prasannah <g.prasannah@gmail.com>
2026-07-16 13:25:13 +0000

    chore: update Homebrew formula to 1.1.7 [skip ci]

--- Formula/kongcheck.rb
+++ Formula/kongcheck.rb
@@ -2,27 +2,27 @@ class Kongcheck < Formula
   desc "CLI tool for detecting Kong Konnect route collisions and shadowing"
   homepage "https://github.com/paambaati/kongcheck"
   license "MIT"
-  version "1.1.6"
+  version "1.1.7"
 
   on_macos do
     on_arm do
-      url "https://github.com/paambaati/kongcheck/releases/download/v1.1.6/kongcheck-darwin-arm64"
-      sha256 "9a2ee8a6bc77edfc2a2935027a33f4253c830f8294b36fdb8c443fb251210df8"
+      url "https://github.com/paambaati/kongcheck/releases/download/v1.1.7/kongcheck-darwin-arm64"
+      sha256 "d2487b3f239f67a2e2f79ac3de01232fc90d8b88c3e35d3ba95f119acb036ff4"
     end
     on_intel do
-      url "https://github.com/paambaati/kongcheck/releases/download/v1.1.6/kongcheck-darwin-x64"
-      sha256 "5c96ae88e531ff1710ddea2d89a5319c7ccec52d18d04a91c8cbea388ede731a"
+      url "https://github.com/paambaati/kongcheck/releases/download/v1.1.7/kongcheck-darwin-x64"
+      sha256 "6d7e02063279e8bac2cf480856cc777cf1e9c7fc13e8c30838557834102400df"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/paambaati/kongcheck/releases/download/v1.1.6/kongcheck-linux-arm64"
-      sha256 "f41e76edffed7a21824fbac1a1a34e5973edbf0b11688b6cc9d6a11d1d5440e3"
+      url "https://github.com/paambaati/kongcheck/releases/download/v1.1.7/kongcheck-linux-arm64"
+      sha256 "19dea21c1b9e588de4f13591b493b5bee84d736aa3491a5840e992574d4a3860"
     end
     on_intel do
-      url "https://github.com/paambaati/kongcheck/releases/download/v1.1.6/kongcheck-linux-x64"
-      sha256 "f19c333a3c8765ed862a17b835e83cfb613a92ccfa392b5137afbafc241ab498"
+      url "https://github.com/paambaati/kongcheck/releases/download/v1.1.7/kongcheck-linux-x64"
+      sha256 "f553e961f6ac46eadd593941d3a841ca63bdd750cd7ae8e8e3d4f18a7dc3d4a2"
     end
   end
 
